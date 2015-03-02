01a27beb1c236746edfe0c432474f4aedd7c1413
gbuj <brandaogbs@gmail.com>
2015-03-02 15:42:58 -0300

    beta 0.01

--- pastebin_filter.py
+++ pastebin_filter.py
@@ -29,3 +29,37 @@ def href_filter():
 
 
 print wget_filter()
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import subprocess
+import shlex
+#import dis
+
+def get_sh(command):#retorna linha (url) por linha pro conn_pbin
+    p = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+    while True:
+        output = p.stdout.readline()
+        if output:
+        	print output.strip()
+	else: 
+		print "escape ;X"
+		break
+
+#def wget_filter():
+#	end_of_pipeu= grep.stdout
+#	print 'SAIDA ENDPIPE:'
+#	for line in end_of_pipe:
+#   		print '\t', line.strip()
+
+def check_content(content):#vai checar o assunto do conn_pbin
+	
+
+
+def conn_pbin(): #vai abrir o site apartir de cada linha do get_sh
+	url = get_sh('cat get_info.sh')
+	
+	
+#print conn_pbin()
+
+
