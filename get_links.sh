9bfb28c3d7fa52b639f814afdfa2a3da8f6f8aa5
gbuj <brandaogbs@gmail.com>
2015-03-02 17:11:17 -0300

    v1.0

--- get_links.sh
+++ get_links.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-
+ 
 rm archive*
-
+ 
 wget http://pastebin.com/archive
 cat archive | grep i_p0 > swap
-
+ 
 cat swap \
-	|sed "s/<a href/\\n<a href/g" \
-	|sed 's/\"/\"><\/a>\n/2' \
-	|grep href \
-	|sed -e :a -e 's/<a href="//g' \
-	|sed "s/\"><\/a>//g" \
-	|uniq \
-	> archive
-
+        |sed "s/<a href/\\n<a href/g" \
+        |sed 's/\"/\"><\/a>\n/2' \
+        |grep href \
+        |sed -e :a -e 's/<a href="//g' \
+        |sed "s/\"><\/a>//g" \
+        |uniq \
+        > archive
+ 
+for i in `cat archive`;
+        do echo http://pastebin.com$i
+done
+ 
+rm archive
 rm swap
